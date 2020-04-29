d10a9cf400e9d0b35848428a10ddc9195c956909
win <1005419755@qq.com>
2020-04-29 21:02:53 +0800

    convertNumberToString

--- week03/convertNumberToString.js
+++ week03/convertNumberToString.js
@@ -1,24 +1,19 @@
-
-const convertNumberToString = (number, hex = 10) => {
+const convertNumberToString = (number, x) => {
   let integer = Math.floor(number);
   let fraction = number - integer;
   let string = '';
 
   while (integer > 0) {
-    string = integer % hex + string;
-    integer = Math.floor(integer / hex);
+    string = String(integer % x) + string;
+    integer = Math.floor(integer / x);
   }
-
   if (fraction > 0) {
-    string += '.';
+    string += ".";
   }
-
-
-  while (fraction > 0) {
-    let integer = Math.floor(fraction * hex);
+  while (fraction >0 && fraction<1) {
+    let integer = Math.floor(fraction * x);
     string += integer;
-    fraction = fraction * hex - integer;
+    fraction = fraction * x - integer;
   }
-
   return string;
 }
