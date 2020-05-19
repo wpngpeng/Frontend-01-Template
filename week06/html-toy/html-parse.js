8fdb7c6abf79a2b6308ac7bcbceff4b727ad2adc
jtr <jtr354@foxmail.com>
2020-05-19 18:42:27 +0800

    week06 html-parse.js 生成dom树

--- week06/html-toy/html-parse.js
+++ week06/html-toy/html-parse.js
@@ -11,229 +11,26 @@
 const EOF = Symbol('EOF') // END OF FILE
 let currentToken
 let currentAttribute
-
-// function emit(token) {
-//   if (token.type !== 'text') {
-//     console.log(token)
-//   }
-// }
-//
-// function data(c) {
-//   if (c === '<') {
-//     return tagOpen
-//   } else if ( c === EOF) {
-//     emit({
-//       type: EOF
-//     })
-//     return false
-//   } else {
-//     emit({
-//       type: 'text',
-//       content: c
-//     })
-//     return data
-//   }
-// }
-// function tagOpen(c) {
-//   if (c === '/') {
-//     return endTagOpen
-//   } else if (c.match(/^[a-zA-Z]$/)) {
-//     currentToken = {
-//       type: 'startTag',
-//       tagName: ''
-//     }
-//     return tagName(c)
-//   } else {
-//     emit({
-//       type: 'text',
-//       content: c
-//     })
-//     return data
-//   }
-// }
-//
-// function tagName(c) {
-//   if (c.match(/^[\t\n\f ]$/)) {
-//     return beforeAttributeName
-//   } else if (c === '/') {
-//     return selfClosingStartTag
-//   } else if (c === '>') {
-//     emit(currentToken)
-//     return data
-//   } else if (c.match(/^[a-zA-Z]$/)) {
-//     currentToken.tagName += c.toLowerCase()
-//     return tagName
-//   } else {
-//     return tagName
-//   }
-// }
-//
-// function beforeAttributeName(c) {
-//   if (c.match(/^[\t\n\f ]$/)) {
-//     return beforeAttributeName
-//   } else if ( c === '>' || c === '/' || c === EOF) {
-//     return afterAttributeName(c)
-//   } else if (c === '=') {
-//   } else {
-//     currentAttribute = {
-//       name: '',
-//       value: ''
-//     }
-//     return attributeName(c)
-//   }
-// }
-//
-// function attributeName(c) {
-//   if (c.match(/^[\t\n\f ]$/ || c === '/' || c === '>' || c === EOF)) {
-//     return afterAttributeName(c)
-//   } else if (c === '=') {
-//     return beforeAttributeName
-//   } else if (c === '\u0000') {
-//
-//   } else if (c === '\"' || c === "'" || c === '<') {
-//
-//   } else {
-//     currentAttribute.name += c
-//     return attributeName
-//   }
-//
-// }
-//
-// function beforeAttributeValue(c) {
-//   if (c.match(/^[\t\n\f ]$/) || c === '/' || c === '>' || c === EOF) {
-//     return beforeAttributeValue
-//   } else if (c === '\"') {
-//     return doubleQuotedAttributeValue
-//   } else if (c === '\'') {
-//     return singleQuotedAttributeValue
-//   } else if (c === '>') {
-//
-//   } else {
-//     return UnqutoedAttributeValue(c)
-//   }
-// }
-//
-// function doubleQuotedAttributeValue(c) {
-//   if (c === "\"") {
-//     currentToken[currentAttribute.name] = currentAttribute.value
-//     return afterQuotedAttributeValue
-//   } else if (c === '\u0000') {
-//
-//   } else if (c === EOF) {
-//
-//   } else {
-//     currentAttribute.value += c
-//     return doubleQuotedAttributeValue
-//   }
-// }
-//
-// function singleQuotedAttributeValue(c) {
-//   if (c === '\'') {
-//     currentToken[currentAttribute.name] = currentAttribute.value
-//     return afterQuotedAttributeValue
-//   }else if (c === '\u0000') {
-//
-//   } else if (c === EOF) {
-//
-//   } else {
-//     currentAttribute.value += c
-//     return singleQuotedAttributeValue
-//   }
-// }
-//
-// function afterQuotedAttributeValue(c) {
-//   if (c.match(/^[\t\n\f ]$/)) {
-//     return beforeAttributeName
-//   } else if (c === '/') {
-//     return selfClosingStartTag
-//   } else if (c === '>') {
-//     currentToken[currentAttribute.name] = currentAttribute.value
-//     emit(currentToken)
-//     return data
-//   } else if (c === EOF) {
-//
-//   } else {
-//     currentAttribute.value += c
-//     return afterQuotedAttributeValue
-//   }
-// }
-//
-//
-// function UnqutoedAttributeValue(c) {
-//   if (c.match(/^[\t\n\f ]$/)) {
-//     currentToken[currentAttribute.name] = currentAttribute.value
-//     return beforeAttributeName
-//   } else if (c==='/') {
-//     currentToken[currentAttribute.name] = currentAttribute.value
-//     return selfClosingStartTag
-//   } else if (c === '>') {
-//     currentToken[currentAttribute.name] = currentAttribute.value
-//     emit(currentToken)
-//     return data
-//   } else if (c==='\u0000') {
-//
-//   } else if (c === '\"' || c === '\'' || c === '<' || c === '=' || c === '`') {
-//   } else if (c === EOF) {
-//   } else {
-//     currentAttribute.value += c
-//     return UnqutoedAttributeValue
-//   }
-// }
-//
-// function afterAttributeName(c) {
-//   if (c.match(/^\t\n\f $/)) {
-//     return afterAttributeName
-//   } else if (c === '/') {
-//     return selfClosingStartTag
-//   } else if (c === '=') {
-//     return beforeAttributeValue
-//   } else if (c === '>') {
-//     currentToken[currentAttribute.name] = currentAttribute.value
-//     emit(currentToken)
-//     return data
-//   } else if (c === EOF) {
-//
-//   } else {
-//     currentToken[currentAttribute.name] = currentAttribute.value
-//     currentAttribute = {
-//       name: '',
-//       value: ''
-//     }
-//     return attributeName(c)
-//   }
-//
-// }
-//
-// function selfClosingStartTag(c) {
-//   if (c === '>') {
-//     currentToken.isSelfClosing = true
-//     emit(currentToken)
-//     return data
-//   } else if (c === EOF) {
-//
-//   } else {
-//     return beforeAttributeName
-//   }
-// }
-//
-// function endTagOpen(c) {
-//   if (c.match(/^[a-zA-Z]$/)) {
-//     currentToken = {
-//       type: 'endTag',
-//       tagName: ''
-//     }
-//     return tagName(c)
-//   } else if (c === '>') {
-//     // return data
-//   } else if (c === EOF) {
-//
-//   } else {
-//
-//   }
-// }
-
+let stack = [{type: 'document', children: []}]
 function emit(token) {
-  console.log(token)
+  // console.log(token)
+  if (token.type === 'startTag') {
+    let element = {
+      type: 'element',
+      tagName: token.tagName,
+      children: [],
+      attributes: []
+    }
+    for (let p in token) {
+      if (p !== 'type' && p !== 'tagName' && p !== 'isSelfClosing') {
+        element.attributes.push({
+          name: p,
+          value: token[p]
+        })
+      }
+    }
+    console.log(element)
+  }
 }
 
 function data (c) {
@@ -279,8 +76,8 @@ function endTagOpen(c) {
 }
 
 function tagName(c) {
-  if (c === /^[\t\n\f ]$/) {
-    throw new Error('beforeAttributeName')
+  if (c.match(/^[\t\n\f ]$/)) {
+    return beforeAttributeName
   } else if (c === '/') {
     return selfClosingStartTag
   } else if (c === '>') {
@@ -308,13 +105,146 @@ function selfClosingStartTag(c) {
   }
 }
 
+function beforeAttributeName(c) {
+  if (c.match(/^[\t\n\f \/>]$/ || c === EOF)){
+    return afterAttributeName(c)
+  } else if (c === '=') {
+    throw new Error('This is an unexpected-equals-sign-before-attribute-name parse error. ')
+  } else {
+    currentAttribute = {
+      name: '',
+      value: ''
+    }
+    return attributeName(c)
+  }
+}
+
+function attributeName(c) {
+  if (c.match(/^[\t\n\f \/>]$/ || c === EOF)) {
+    return afterAttributeName(c)
+  } else if (c === '=') {
+    return beforeAttributeValue
+  } else if (c.match(/^["'<]$/)){
+    throw new Error('This is an unexpected-character-in-attribute-name parse error. ')
+  } else {
+    currentAttribute.name += c
+    return attributeName
+  }
+}
+
+function afterAttributeName(c) {
+  if (c.match(/^[\t\n\f ]$/)) {
+    return afterAttributeName
+  } else if (c === '/') {
+    return selfClosingStartTag
+  } else if (c === '=') {
+    return beforeAttributeValue
+  } else if (c === '>') {
+    currentToken[currentAttribute.name] = currentAttribute.value
+    emit(currentToken)
+    return data
+  } else if (c === EOF) {
+    throw new Error('This is an eof-in-tag parse error. ')
+  } else {
+    currentAttribute = {
+      name: '',
+      value: ''
+    }
+    return attributeName
+  }
+}
+
+function beforeAttributeValue(c) {
+  if(c.match(/^[\t\n\f ]$/)) {
+    return beforeAttributeValue
+  } else if (c === '"') {
+    return  doubleQuotedAttributeValue
+  } else if (c === '\'') {
+    return singleQuotedAttributeValue
+  } else if (c === '>') {
+    throw new Error('This is a missing-attribute-value parse error. ')
+  } else {
+    return unQuotedAttributeValue
+  }
+}
+
+function doubleQuotedAttributeValue(c) {
+  if (c === '"') {
+    currentToken[currentAttribute.name] = currentAttribute.value
+    return afterAttributeValueQuoted
+  } else if (c === '&') {
+    currentAttribute.value += c
+    return doubleQuotedAttributeValue
+  } else if (c === EOF) {
+    throw new Error('This is an eof-in-tag parse error.')
+  } else {
+    currentAttribute.value += c
+    return doubleQuotedAttributeValue
+  }
+}
+
+function singleQuotedAttributeValue(c) {
+  if (c === '\'') {
+    currentToken[currentAttribute.name] = currentAttribute.value
+    return afterAttributeValueQuoted
+  } else if (c === '&') {
+    currentAttribute.value += c
+    return doubleQuotedAttributeValue
+  } else if (c === EOF) {
+    throw new Error('This is an eof-in-tag parse error.')
+  } else {
+    currentAttribute.value += c
+    return singleQuotedAttributeValue
+  }
+}
+
+function unQuotedAttributeValue(c) {
+  if (c.match(/^[\t\n\f ]$/)) {
+    currentToken[currentAttribute.name] = currentAttribute.value
+    return beforeAttributeName
+  } else if(c === '&') {
+    currentAttribute.value += c
+    return unQuotedAttributeValue
+  } else if (c === '>') {
+    currentToken[currentAttribute.name] = currentAttribute.value
+    emit(currentToken)
+    return data
+  } else if (c.match(/^["'<=`]$/)) {
+    throw new Error('This is an unexpected-character-in-unquoted-attribute-value parse error.')
+  } else if (c === EOF) {
+    throw new Error('This is an eof-in-tag parse error. ')
+  } else {
+    currentAttribute.value += c
+    return unQuotedAttributeValue
+  }
+}
+
+function afterAttributeValueQuoted(c) {
+  if (c.match(/^[\t\n\f ]$/)) {
+    return beforeAttributeName
+  } else if (c === '/') {
+    return selfClosingStartTag
+  } else if (c === '>') {
+    currentToken[currentAttribute.name] = currentAttribute.value
+    emit(currentToken)
+    return data
+  } else if (c === EOF) {
+    throw new Error('This is an eof-in-tag parse error. ')
+  } else {
+    throw new Error('This is a missing-whitespace-between-attributes parse error.')
+  }
+}
+
+
+
 module.exports = function parserHTML(html) {
   let state = data
-  console.log(html)
   for (let c of html) {
+    // console.log(c)
     state = state(c)
   }
   state = state(EOF)
+  console.log(JSON.stringify(stack[0], ' ', 2))
   return {}
 }
 
