ef969c689a8bf3ba5496cbf1c386fbfcca80fe5a
jtr354 <jtr354@foxmail.com>
2020-06-04 06:51:07 +0800

    week08

--- week08/tools.js
+++ week08/tools.js
@@ -8,7 +8,25 @@
 // [0,0,1,1]
 
 function match(selector, element) {
+  // 1. 分析选择器列表
+  // 2. 分析combinator
+  // 3. 分析singleSelector
+  const selectors = selector.split(',')
+  selectors.forEach((selector) => {
+    combinator(selector.trim())
+  })
   return true
 }
 
-match('div #id.class', document.getElementById('id'))
+function combinator(selector) {
+  for(let c of selector) {
+      getAST(c)
+  }
+}
+
+function getAST(c) {
+  console.log(c)
+}
+
+// match('div #id.class', document.getElementById('id'))
+match('div #id.class')
